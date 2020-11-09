3c3af2592b8d2cde74336750af0fd99aa8be5f11
Alka <ajit.alka@gmail.com>
2020-11-09 02:05:38 -0500

    adding new classifiers to road-obj-detection and a new test image

--- road-obj-detection.py
+++ road-obj-detection.py
@@ -6,6 +6,11 @@ img_file = 'test_image.jpg'
 # Pre-trained car classifier
 car_classifier_file = 'classifiers/cars.xml'
 ped_classifier_file = 'classifiers/pedestrians.xml'
+#bike_classifier_file = 'classifiers/bike.xml' #backup
+bus_classifier_file = 'classifiers/bus.xml'
+bicycle_classifier_file = 'classifiers/bicycle.xml'
+motorbike_classifier_file = 'classifiers/motorbike.xml'
+
 
 # Create opencv image
 img = cv2.imread(img_file)
@@ -13,6 +18,10 @@ img = cv2.imread(img_file)
 # Create classifier
 car_tracker = cv2.CascadeClassifier(car_classifier_file)
 ped_tracker = cv2.CascadeClassifier(ped_classifier_file)
+#bike_tracker = cv2.CascadeClassifier(bike_classifier_file)
+bus_tracker = cv2.CascadeClassifier(bus_classifier_file)
+bicycle_tracker = cv2.CascadeClassifier(bicycle_classifier_file)
+motorbike_tracker = cv2.CascadeClassifier(motorbike_classifier_file)
 
 # Convert to Grayscale
 black_n_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -22,6 +31,9 @@ black_n_white = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # detect cars
 cars = car_tracker.detectMultiScale(black_n_white)
 pedestrians = ped_tracker.detectMultiScale(black_n_white)
+buses = bus_tracker.detectMultiScale(black_n_white)
+bicycles = bicycle_tracker.detectMultiScale(black_n_white)
+motorbikes = motorbike_tracker.detectMultiScale(black_n_white)
 
 print(pedestrians)
 
@@ -31,6 +43,15 @@ for (x, y, w, h) in cars:
 
 for (x, y, w, h) in pedestrians:
     cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
+
+for (x, y, w, h) in buses:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (0, 0, 255), 2)
+
+for (x, y, w, h) in bicycles:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (51, 199, 95), 2)
+
+for (x, y, w, h) in motorbikes:
+    cv2.rectangle(img, (x,y), (x+w, y+h), (102, 0, 204), 2) 
 
 cv2.imshow('Car and Pedestrian Detector', img)
 cv2.waitKey()
